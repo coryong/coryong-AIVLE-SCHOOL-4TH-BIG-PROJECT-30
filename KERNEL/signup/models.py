2c6622499a923f5a2f7fb205a70378dc116147f4
hyunjin <dhguswls5028@naver.com>
2023-12-18 14:52:53 +0900

    db접근허용3

--- KERNEL/signup/models.py
+++ KERNEL/signup/models.py
@@ -6,11 +6,11 @@ from django.db import models
 class UserTable(models.Model):
     username = models.CharField(max_length=50)
     password = models.CharField(max_length=255)
-    name = models.CharField(max_length=100, blank=True)
-    phone = models.CharField(max_length=20, blank=True)
-    email = models.CharField(max_length=100, blank=True)
-    area = models.CharField(max_length=50, blank=True)
-    gender = models.CharField(max_length=10, blank=True)
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField(max_length=100)
+    area = models.CharField(max_length=50)
+    gender = models.CharField(max_length=10)
 
     def __str__(self):
         return self.username
