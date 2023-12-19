b71a575eb426df6ed186b445cc1ae7c93ec5ecf7
taehyeong <steve181813@gmail.com>
2023-12-19 10:23:57 +0900

    test_db

---
+++ KERNEL/my_setting.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bigproject30', # db이름
+        'USER': 'taehyeong', # 로그인-유저 명
+        'PASSWORD': 'aivle30',# 로그인- 비밀번호
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
--- KERNEL/mysite/settings.py
+++ KERNEL/mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import my_setting
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'post',
 ]
 
 MIDDLEWARE = [
@@ -73,12 +75,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = my_setting.DATABASES
 
 
 # Password validation
