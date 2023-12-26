a4cafa26e899e3d975ec858a97ef6bb23b5cbc91
sunbeen0111@naver.com <sunbeen0111@naver.com>
2023-12-26 13:30:59 +0900

    기술스택db 다중선택 추가

--- KERNEL/post/admin.py
+++ KERNEL/post/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Post
+# Register your models here.
+# TechnologyStack 모델을 관리자 사이트에 등록합니다.
+admin.site.register(Post)
--- KERNEL/user/views.py
+++ KERNEL/user/views.py
@@ -7,3 +7,4 @@ from rest_framework import generics
 class UserCreate(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    
